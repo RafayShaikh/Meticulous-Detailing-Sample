d002416f6e6361351c8384b32d68c5d59c7f9ecd
RafayShaikh <rafaysarr@gmail.com>
2021-08-30 18:53:42 -0500

    Minor fixes

--- MD/components/PPF/PPF.js
+++ MD/components/PPF/PPF.js
@@ -84,7 +84,7 @@ function PPF({ setPage }) {
         <svg
           onClick={() => setPage(0)}
           xmlns='http://www.w3.org/2000/svg'
-          class='h-6 w-6'
+          className='h-6 w-6'
           fill='none'
           viewBox='0 0 24 24'
           stroke='currentColor'
